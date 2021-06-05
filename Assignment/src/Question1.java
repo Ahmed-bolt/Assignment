78a8aeaa9388b781c7f33c4ae2d9e3d5d887e79b
Ahmed <iykebilzz@gmail.com>
2021-06-05 03:45:33 -0700

    my first commit

---
+++ Assignment/src/Question1.java
@@ -0,0 +1,19 @@
+
+
+public class Question1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int variable1;
+        int variable2, variable3;
+        
+        variable1=9;
+        variable2=24;
+        variable3=16;
+        		
+        		System.out.println("The value is variable is:" + variable1);
+        		System.out.println("Variable2 is" + variable2 + ", Variable3 is " + variable3);
+        
+	}
+
+}
